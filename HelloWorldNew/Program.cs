5e75d503b3f0e0aae61dd73b3a1f6397db13451f
Juan Bautista Arambarri <jbat1997@gmail.com>
2024-05-01 10:57:17 -0300

    Try concatenation and other sillies

--- HelloWorldNew/Program.cs
+++ HelloWorldNew/Program.cs
@@ -6,7 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string hello = "Hello World";
+            Console.WriteLine(hello);
+            Console.ReadLine();
+            Console.WriteLine("Beep");
+            Console.WriteLine("I SAID " + hello.ToUpper());
+            Console.Beep();
+            Console.WriteLine("Boop");
+            Console.WriteLine("Invariant Upper " + hello.ToUpperInvariant());
+            Console.WriteLine("Invariant Lower " + hello.ToLowerInvariant());
+
+            int num1 = 7, num2 = 5, num3 = 1;
+            
+            // Console.WriteLine("The numbers saved are: " + num1 +" "+ num2 +" "+ num3);
         }
     }
 }
