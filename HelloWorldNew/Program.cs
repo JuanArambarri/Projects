c2786375668401807cb2be1f8e70fbcaffa181d2
Juan Bautista Arambarri <jbat1997@gmail.com>
2024-05-06 09:40:45 -0300

    Mon 05/06
    Parsing and other tries for Udemy Course

--- HelloWorldNew/Program.cs
+++ HelloWorldNew/Program.cs
@@ -4,21 +4,50 @@ namespace HelloWorld
 {
     class Program
     {
+        // www.dofactory.com/reference/csharp-coding-standards
         static void Main(string[] args)
         {
             string hello = "Hello World";
             Console.WriteLine(hello);
-            Console.ReadLine();
-            Console.WriteLine("Beep");
-            Console.WriteLine("I SAID " + hello.ToUpper());
-            Console.Beep();
-            Console.WriteLine("Boop");
-            Console.WriteLine("Invariant Upper " + hello.ToUpperInvariant());
-            Console.WriteLine("Invariant Lower " + hello.ToLowerInvariant());
-
-            int num1 = 7, num2 = 5, num3 = 1;
+            string userInput = Console.ReadLine();
             
-            // Console.WriteLine("The numbers saved are: " + num1 +" "+ num2 +" "+ num3);
+            //Implicit conversion (from smaller data type to bigger)
+            int num = 123456708;
+            long bigNum = num;
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+            
+            //Casting [Explicit conversion]
+            double myDouble = 13.37;
+            int myInt;
+            
+            //type conversion
+            string myString = myDouble.ToString(); //  "13.37"
+            myFloat.ToString();
+
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+            // Will forcibly transform myDouble to int. Prints 13.
+            
+            
+                /* {0} is replaced by the variable placed after the comma. 
+                Can also take more inputs using {n+1}
+                view example below */
+            Console.WriteLine("You have entered {0} {1}", userInput,hello);
+
+            string myString1 = "13";
+
+            // if myString is not parseable, it will throw an exemption.
+            int _num = Int32.Parse(myString1);
+            
+            int num1 = 7, num2 = 5, num3 = 1;
+            Console.WriteLine("The numbers saved are: " + num1 +" "+ num2 +" "+ num3 + "\n"+ userInput);
         }
     }
 }
+
+
+//<summary>
+//This is a piece of useless code
+//</summary>
+//XML style commenting
