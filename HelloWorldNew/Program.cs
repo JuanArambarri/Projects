1941d70a74b45606510de5906b4c9bac633047da
Juan Bautista Arambarri <jbat1997@gmail.com>
2024-05-08 19:48:09 -0300

    Implement string exercise
    My solution to Udemy C# course string exercise.

--- HelloWorldNew/Program.cs
+++ HelloWorldNew/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading.Channels;
 
 namespace HelloWorld
 
@@ -9,6 +10,18 @@ namespace HelloWorld
         // www.dofactory.com/reference/csharp-coding-standards
         static void Main(string[] args)
         {
+         string myName;
+         Console.Write("Please enter your name and press enter: ");
+         myName = Console.ReadLine();
+         string inputToUpper = myName.ToUpper();
+         string inputToLower = myName.ToLower();
+         string inputNoWhitespace = myName.Trim();
+         var inputSubString = myName.Substring(0,2);
+
+         Console.WriteLine("Solutions:{0} \n{1} \n{2} \n{3}", inputToUpper, inputToLower, inputNoWhitespace, inputSubString);
+         
+         //Console.WriteLine("solutions: " + inputToUpper + "\n" + inputToLower + "\n" + inputNoWhitespace + "\n" + inputSubString);
+         
            //Console.WriteLine("String concatenation");
            /*
             int age = 31;
@@ -32,31 +45,36 @@ namespace HelloWorld
             * Console.WriteLine(@"Muahaha \n you have no powers here!.");
             */
            
+           /*
+            * escape character \.
+            * string s1 = "this is \"something\"";
+            * Console.WriteLine(s1);
+            */
             
-            string stringForFloat = "0.85";
-            string stringForInt = "12345";
-            Console.WriteLine(Int32.Parse(stringForInt) + " " + float.Parse(stringForFloat));
+            //string stringForFloat = "0.85";
+            //string stringForInt = "12345";
+            //Console.WriteLine(Int32.Parse(stringForInt) + " " + float.Parse(stringForFloat));
 
-            string hello = "Hello World";
-            Console.WriteLine(hello);
-            string userInput = Console.ReadLine();
+            //string hello = "Hello World";
+            //Console.WriteLine(hello);
+            //string userInput = Console.ReadLine();
 
             //Implicit conversion (from smaller data type to bigger)
-            int num = 123456708;
-            long bigNum = num;
-            float myFloat = 13.37f;
-            double myNewDouble = myFloat;
+            //kint num = 123456708;
+            //long bigNum = num;
+            //float myFloat = 13.37f;
+            //double myNewDouble = myFloat;
 
             //Casting [Explicit conversion]
-            double myDouble = 13.37;
-            int myInt;
+            //double myDouble = 13.37;
+            //int myInt;
 
             //type conversion
-            string myString = myDouble.ToString(); //  "13.37"
-            myFloat.ToString();
+            //string myString = myDouble.ToString(); //  "13.37"
+            //myFloat.ToString();
 
-            myInt = (int)myDouble;
-            Console.WriteLine(myInt);
+            //myInt = (int)myDouble;
+            //Console.WriteLine(myInt);
             // Will forcibly transform myDouble to int. Prints 13.
 
             /* {0} is replaced by the variable placed after the comma.
