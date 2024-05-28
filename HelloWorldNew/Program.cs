fe643cf322f5c7803822b65c81e302de8b053b35
Juan Bautista Arambarri <jbat1997@gmail.com>
2024-05-28 09:22:43 -0300

    Implement new Class
    starting WeatherSimulator class for "Method" Exercise [Udemy]

--- HelloWorldNew/Program.cs
+++ HelloWorldNew/Program.cs
@@ -8,7 +8,7 @@ namespace HelloWorld
  class Program
  {
   // www.dofactory.com/reference/csharp-coding-standards
-
+  //variables created before Main will be a "Field", meaning it is usable in the whole program.
   static void Main(string[] args)
   {
    TicTacToeTheGame.TicTacToe();
---
+++ HelloWorldNew/WeatherSimulator.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld;
+
+public class WeatherSimulator
+{
+    public static void Simulator()
+    {
+        Console.WriteLine("Please input the amount of days to simulate weather for.");
+        int days = int.Parse(Console.ReadLine());
+        int[] temperature = new int[days];
+        string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+        string[] weatherConditions = new string[days];
+
+        Random random = new Random();
+        for (int i = 0; i < days; i++)
+        {
+            temperature[i] = random.Next(-10, 40);
+            weatherConditions[i] = conditions[random.Next(random.Next(conditions.Length))];
+            Console.WriteLine(temperature[i] + " " + weatherConditions[i]);
+        }
+    }
+}
